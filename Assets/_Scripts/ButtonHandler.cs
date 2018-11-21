f05d3e2477a3c185f9911cc2ae3285faeb64f504
DESKTOP-IB7C6IK\Steven <thedankpure@gmail.com>
2018-11-22 05:08:32 +0900

    added buttons + boxes

---
+++ Assets/_Scripts/ButtonHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour {
+
+    [SerializeField]
+    private Sprite onSprite;
+
+    private Sprite offSprite;
+
+    private SpriteRenderer spr;
+    private BoxCollider2D bCol2D;
+
+    [SerializeField]
+    private GameObject door;
+
+	void Start () {
+        spr = GetComponent<SpriteRenderer>();
+        bCol2D = GetComponent<BoxCollider2D>();
+        offSprite = spr.sprite;
+	}
+
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        spr.sprite = onSprite;
+        door.SetActive(true);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        spr.sprite = offSprite;
+        door.SetActive(false);
+    }
+}
