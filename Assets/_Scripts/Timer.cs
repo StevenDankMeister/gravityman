38ac50523c31a261d30ac97c78460367aae825ca
Steven <thedankpure@gmail.com>
2018-11-22 16:24:42 +0900

    added timer

---
+++ Assets/_Scripts/Timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    private float time;
+    private Text timerText;
+
+	void Start () {
+        time = Time.time;
+        timerText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timerText.text = "Time: " + time;
+        time = Mathf.FloorToInt(Time.time);
+	}
+}
